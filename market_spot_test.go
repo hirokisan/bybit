30b783ef7d0e2f4e4f6301d109cebd403cf38986
hirokisan <dev@hirokiueno.com>
2021-12-02 22:24:02 +0900

    test: post spot-v1-order

--- market_spot_test.go
+++ market_spot_test.go
@@ -136,3 +136,25 @@ func TestSpotQuoteTickerBookTicker(t *testing.T) {
 		testhelper.UpdateFile(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
 	}
 }
+
+func TestSpotPostOrder(t *testing.T) {
+	client := NewTestClient().WithAuthFromEnv()
+	price := 28383.5
+	res, err := client.Market().SpotPostOrder(SpotPostOrderParam{
+		Symbol: SymbolSpotBTCUSDT,
+		Qty:    0.01,
+		Side:   SideBuy,
+		Type:   OrderTypeSpotLimit,
+		Price:  &price,
+	})
+	{
+		require.NoError(t, err)
+		require.Equal(t, "", res.RetMsg)
+	}
+	{
+		goldenFilename := "./testdata/spot-v1-post-order.json"
+		testhelper.Compare(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
+		testhelper.UpdateFile(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
+	}
+	// TODO : cancel order
+}
