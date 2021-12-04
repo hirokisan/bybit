5e2c007426fd51dc86392d78dfcbc99b094a14df
hirokisan <dev@hirokiueno.com>
2021-12-04 12:31:34 +0900

    test: spot-v1-order-batch-cancel

--- market_spot_test.go
+++ market_spot_test.go
@@ -283,3 +283,37 @@ func TestSpotDeleteFastOrder(t *testing.T) {
 		testhelper.UpdateFile(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
 	}
 }
+
+func TestSpotOrderBatchCancel(t *testing.T) {
+	client := NewTestClient().WithAuthFromEnv()
+
+	var symbol SymbolSpot
+	{
+		price := 28383.5
+		res, err := client.Market().SpotPostOrder(SpotPostOrderParam{
+			Symbol: SymbolSpotBTCUSDT,
+			Qty:    0.01,
+			Side:   SideBuy,
+			Type:   OrderTypeSpotLimit,
+			Price:  &price,
+		})
+		{
+			require.NoError(t, err)
+			require.Equal(t, "", res.RetMsg)
+		}
+		symbol = SymbolSpot(res.Result.Symbol)
+	}
+
+	res, err := client.Market().SpotOrderBatchCancel(SpotOrderBatchCancelParam{
+		Symbol: symbol,
+	})
+	{
+		require.NoError(t, err)
+		require.Equal(t, "", res.RetMsg)
+	}
+	{
+		goldenFilename := "./testdata/spot-v1-order-batch-cancel.json"
+		testhelper.Compare(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
+		testhelper.UpdateFile(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
+	}
+}
