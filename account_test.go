5280638c70adf54d526793d03fe0268d02de6cb6
hirokisan <dev@hirokiueno.com>
2022-06-20 22:39:23 +0900

    test: get v2-private-order-list

--- account_test.go
+++ account_test.go
@@ -55,6 +55,64 @@ func TestCreateOrder(t *testing.T) {
 	})
 }
 
+func TestListOrder(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
+		client := NewTestClient().WithAuthFromEnv()
+		var orderID string
+		{
+			price := 10000.0
+			res, err := client.Account().CreateOrder(CreateOrderParam{
+				Side:        SideBuy,
+				Symbol:      SymbolInverseBTCUSD,
+				OrderType:   OrderTypeLimit,
+				Qty:         1,
+				TimeInForce: TimeInForceGoodTillCancel,
+				Price:       &price,
+			})
+			{
+				require.NoError(t, err)
+				require.Equal(t, "OK", res.RetMsg)
+			}
+			orderID = res.Result.OrderID
+		}
+		res, err := client.Account().ListOrder(ListOrderParam{
+			Symbol: SymbolInverseBTCUSD,
+		})
+		{
+			require.NoError(t, err)
+			require.Equal(t, "OK", res.RetMsg)
+		}
+		{
+			goldenFilename := "./testdata/v2-private-order-list.json"
+			testhelper.Compare(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
+			testhelper.UpdateFile(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
+		}
+		// clean
+		{
+			res, err := client.Account().CancelOrder(CancelOrderParam{
+				Symbol:  SymbolInverseBTCUSD,
+				OrderID: &orderID,
+			})
+			require.NoError(t, err)
+			require.Equal(t, "OK", res.RetMsg)
+		}
+	})
+
+	t.Run("auth error", func(t *testing.T) {
+		client := NewTestClient()
+		price := 28383.5
+		_, err := client.Account().CreateOrder(CreateOrderParam{
+			Side:        SideBuy,
+			Symbol:      SymbolInverseBTCUSD,
+			OrderType:   OrderTypeLimit,
+			Qty:         1,
+			TimeInForce: TimeInForceGoodTillCancel,
+			Price:       &price,
+		})
+		require.Error(t, err)
+	})
+}
+
 func TestListPosition(t *testing.T) {
 	t.Run("ok", func(t *testing.T) {
 		client := NewTestClient().WithAuthFromEnv()
