5b47f2e7f012efbc4c4a5b8326384faaf6f930c5
hirokisan <dev@hirokiueno.com>
2022-07-29 22:36:42 +0900

    test: add test helper

---
+++ testhelper/server.go
@@ -0,0 +1,33 @@
+package testhelper
+
+import (
+	"net/http"
+	"net/http/httptest"
+)
+
+// NewServer :
+func NewServer(opts ...func(*http.ServeMux)) (*httptest.Server, func()) {
+	mux := http.NewServeMux()
+	for _, opt := range opts {
+		opt(mux)
+	}
+	ts := httptest.NewServer(mux)
+	return ts, ts.Close
+}
+
+// WithHandlerOption :
+func WithHandlerOption(
+	path, method string,
+	status int,
+	respBody []byte,
+) func(*http.ServeMux) {
+	return func(mux *http.ServeMux) {
+		mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			if r.Method == method {
+				_, _ = w.Write(respBody)
+				w.WriteHeader(status)
+			}
+		})
+	}
+}
