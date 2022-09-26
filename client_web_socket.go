daf25d452e9b668189d60892fb538788a93a188d
hirokisan <dev@hirokiueno.com>
2022-09-26 22:30:49 +0900

    feat: start with multiple subscriptions

--- client_web_socket.go
+++ client_web_socket.go
@@ -1,5 +1,13 @@
 package bybit
 
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+)
+
 const (
 	// WebsocketScheme :
 	WebsocketScheme = "wss"
@@ -27,4 +35,64 @@ func (s *SpotWebsocketService) V1() *SpotWebsocketV1Service {
 // Spot :
 func (c *WebSocketClient) Spot() *SpotWebsocketService {
 	return &SpotWebsocketService{}
+}
+
+// WebsocketExecutor :
+type WebsocketExecutor interface {
+	Run() error
+	Close() error
+	Ping() error
+}
+
+// Start :
+func (c *WebSocketClient) Start(ctx context.Context, executors []WebsocketExecutor) {
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+
+		for {
+			for _, executor := range executors {
+				if err := executor.Run(); err != nil {
+					if IsErrWebsocketClosed(err) {
+						return
+					}
+					log.Println(err)
+					return
+				}
+			}
+		}
+	}()
+
+	ticker := time.NewTicker(20 * time.Second)
+	defer ticker.Stop()
+
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt)
+	defer stop()
+
+	for {
+		select {
+		case <-done:
+			return
+		case <-ticker.C:
+			for _, executor := range executors {
+				if err := executor.Ping(); err != nil {
+					return
+				}
+			}
+		case <-ctx.Done():
+			log.Println("interrupt")
+
+			for _, executor := range executors {
+				if err := executor.Close(); err != nil {
+					return
+				}
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
 }
