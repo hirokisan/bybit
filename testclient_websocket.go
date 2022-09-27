202b3dfcd558efeeb3f792a974e8edfced5515c5
hirokisan <dev@hirokiueno.com>
2022-09-27 22:40:47 +0900

    test: integrationtest base for spov1

---
+++ testclient_websocket.go
@@ -0,0 +1,38 @@
+package bybit
+
+import "os"
+
+const (
+	// TestWebsocketHost :
+	TestWebsocketHost = "stream-testnet.bybit.com"
+)
+
+// TestWebSocketClient :
+type TestWebSocketClient struct {
+	*WebSocketClient
+}
+
+// NewTestWebsocketClient :
+func NewTestWebsocketClient() *TestWebSocketClient {
+	return &TestWebSocketClient{
+		WebSocketClient: &WebSocketClient{
+			baseHost: TestWebsocketHost,
+		},
+	}
+}
+
+// WithAuthFromEnv :
+func (c *TestWebSocketClient) WithAuthFromEnv() *TestWebSocketClient {
+	key, ok := os.LookupEnv("BYBIT_TEST_KEY")
+	if !ok {
+		panic("need BYBIT_TEST_KEY as environment variable")
+	}
+	secret, ok := os.LookupEnv("BYBIT_TEST_SECRET")
+	if !ok {
+		panic("need BYBIT_TEST_SECRET as environment variable")
+	}
+	c.key = key
+	c.secret = secret
+
+	return c
+}
