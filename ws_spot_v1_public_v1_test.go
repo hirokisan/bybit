9cba9680721af57e1c8060f727799afb8e089699
hirokisan <dev@hirokiueno.com>
2022-09-29 09:20:06 +0900

    test: spot v1 public v1 trade

---
+++ ws_spot_v1_public_v1_test.go
@@ -0,0 +1,59 @@
+package bybit
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/hirokisan/bybit/testhelper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSpotWebsocketV1PublicV1Trade(t *testing.T) {
+	respBody := SpotWebsocketV1PublicV1TradeResponse{
+		Symbol:         SymbolSpotBTCUSDT,
+		SymbolName:     string(SymbolSpotBTCUSDT),
+		Topic:          SpotWebsocketV1PublicV1TopicTrade,
+		SendTime:       1664284020685,
+		IsFirstMessage: true,
+		Params: SpotWebsocketV1PublicV1TradeResponseParams{
+			RealtimeInterval: "24h",
+			Binary:           "false",
+		},
+		Data: []SpotWebsocketV1PublicV1TradeContent{
+			{
+				TradeID:        "2100000000002571479",
+				Timestamp:      1664283342503,
+				Price:          "20191.69",
+				Quantity:       "0.000495",
+				IsBuySideTaker: true,
+			},
+		},
+	}
+	bytesBody, err := json.Marshal(respBody)
+	require.NoError(t, err)
+
+	server, teardown := testhelper.NewWebsocketServer(
+		testhelper.WithWebsocketHandlerOption(SpotWebsocketV1PublicV1Path, bytesBody),
+	)
+	defer teardown()
+
+	wsClient := NewTestWebsocketClient().
+		WithBaseURL(server.URL)
+
+	svc, err := wsClient.Spot().V1().PublicV1()
+	require.NoError(t, err)
+
+	unsubscribe, err := svc.SubscribeTrade(SymbolSpotBTCUSDT, func(response SpotWebsocketV1PublicV1TradeResponse) error {
+		fmt.Printf("%+v", response)
+		assert.Equal(t, respBody, response)
+		return nil
+	})
+	require.NoError(t, err)
+
+	assert.NoError(t, svc.Run())
+	assert.NoError(t, unsubscribe())
+	assert.NoError(t, svc.Ping())
+	assert.NoError(t, svc.Close())
+}
