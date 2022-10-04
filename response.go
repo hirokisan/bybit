f5081210458d9b3813e28c85fdced766317f1b13
hirokisan <dev@hirokiueno.com>
2022-10-05 08:52:20 +0900

    handle error response (#18)

--- response.go
+++ response.go
@@ -1,5 +1,11 @@
 package bybit
 
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
 // CommonResponse :
 type CommonResponse struct {
 	RetCode          int    `json:"ret_code"`
@@ -11,3 +17,19 @@ type CommonResponse struct {
 	RateLimitResetMs int    `json:"rate_limit_reset_ms"`
 	RateLimit        int    `json:"rate_limit"`
 }
+
+// RateLimitError :
+type RateLimitError struct {
+	*CommonResponse `json:",inline"`
+}
+
+func (r *RateLimitError) Error() string {
+	return fmt.Sprintf("%s, %s", r.RetMsg, time.Until(time.Unix(int64(r.RateLimitResetMs/1000), 0)))
+}
+
+var (
+	// ErrPathNotFound : Request path not found
+	ErrPathNotFound = errors.New("path not found")
+	// ErrAccessDenied : Access denied
+	ErrAccessDenied = errors.New("access denied")
+)
