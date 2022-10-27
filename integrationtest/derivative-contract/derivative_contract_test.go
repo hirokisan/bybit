81c5d64904114de0caa6ecb748bcd364baffd7e5
hirokisan <dev@hirokiueno.com>
2022-10-27 22:32:35 +0900

    test: integration

--- integrationtest/derivative-contract/derivative_contract_test.go
+++ integrationtest/derivative-contract/derivative_contract_test.go
@@ -23,3 +23,20 @@ func TestDerivativesOrderBook(t *testing.T) {
 		testhelper.UpdateFile(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
 	}
 }
+
+func TestDerivativesKline(t *testing.T) {
+	client := bybit.NewTestClient()
+	res, err := client.Derivative().Contract().DerivativesKline(bybit.DerivativesKlineParam{
+		Symbol:   bybit.SymbolDerivativeBTCUSDT,
+		Category: bybit.CategoryDerivativeLinear,
+		Interval: bybit.IntervalD,
+		Start:    1652112000000,
+		End:      1652544000000,
+	})
+	require.NoError(t, err)
+	{
+		goldenFilename := "./testdata/derivatives-public-kline.json"
+		testhelper.Compare(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
+		testhelper.UpdateFile(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
+	}
+}
