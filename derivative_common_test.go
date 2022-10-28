32daa07634c782cf2fd21544a9ea0871be34ba9c
hirokisan <dev@hirokiueno.com>
2022-10-28 18:58:30 +0900

    test: unit

--- derivative_common_test.go
+++ derivative_common_test.go
@@ -103,3 +103,117 @@ func TestDerivativesKline(t *testing.T) {
 	assert.Equal(t, respBody["result"].(map[string]interface{})["symbol"], string(resp.Result.Symbol))
 	assert.Equal(t, respBody["result"].(map[string]interface{})["list"].([][]string)[0][0], resp.Result.Lists[0].Start)
 }
+
+func TestDerivativesTickers(t *testing.T) {
+	param := DerivativesTickersParam{
+		Category: CategoryDerivativeLinear,
+	}
+
+	path := "/derivatives/v3/public/tickers"
+	method := http.MethodGet
+	status := http.StatusOK
+	respBody := map[string]interface{}{
+		"result": map[string]interface{}{
+			"category": "linear",
+			"list": []map[string]string{
+				{
+					"symbol":                 "ZRXUSDT",
+					"bidPrice":               "0.252",
+					"askPrice":               "0.2522",
+					"lastPrice":              "0.2497",
+					"lastTickDirection":      "MinusTick",
+					"prevPrice24h":           "0.2638",
+					"price24hPcnt":           "-0.053449",
+					"highPrice24h":           "0.2645",
+					"lowPrice24h":            "0.2497",
+					"prevPrice1h":            "0.2530",
+					"markPrice":              "0.2522",
+					"indexPrice":             "0.2524",
+					"openInterest":           "516350",
+					"turnover24h":            "1439.724",
+					"volume24h":              "5474",
+					"fundingRate":            "0.0001",
+					"nextFundingTime":        "1666972800000",
+					"predictedDeliveryPrice": "",
+					"basisRate":              "",
+					"deliveryFeeRate":        "",
+					"deliveryTime":           "0",
+				},
+			},
+		},
+	}
+	bytesBody, err := json.Marshal(respBody)
+	require.NoError(t, err)
+
+	server, teardown := testhelper.NewServer(
+		testhelper.WithHandlerOption(path, method, status, bytesBody),
+	)
+	defer teardown()
+
+	client := NewTestClient().
+		WithBaseURL(server.URL)
+
+	resp, err := client.Derivative().UnifiedMargin().DerivativesTickers(param)
+	require.NoError(t, err)
+
+	require.NotNil(t, resp)
+	assert.Equal(t, respBody["result"].(map[string]interface{})["category"], string(resp.Result.Category))
+	assert.Equal(t, respBody["result"].(map[string]interface{})["list"].([]map[string]string)[0]["symbol"], string(resp.Result.Lists[0].Symbol))
+}
+
+func TestDerivativesTickersForOption(t *testing.T) {
+	param := DerivativesTickersForOptionParam{
+		Symbol: SymbolDerivativeBTC31MAR23_40000C,
+	}
+
+	path := "/derivatives/v3/public/tickers"
+	method := http.MethodGet
+	status := http.StatusOK
+	respBody := map[string]interface{}{
+		"result": map[string]string{
+			"category":               "option",
+			"symbol":                 "BTC-31MAR23-40000-C",
+			"bidPrice":               "270",
+			"bidSize":                "19.9",
+			"bidIv":                  "0.6514",
+			"askPrice":               "325",
+			"askSize":                "45.9",
+			"askIv":                  "0.6778",
+			"lastPrice":              "225",
+			"highPrice24h":           "0",
+			"lowPrice24h":            "0",
+			"markPrice":              "277.11257031",
+			"indexPrice":             "20193.37",
+			"markPriceIv":            "0.655",
+			"underlyingPrice":        "20259.98",
+			"openInterest":           "4.63",
+			"turnover24h":            "0",
+			"volume24h":              "0",
+			"totalVolume":            "56",
+			"totalTurnover":          "1086495",
+			"delta":                  "0.08275356",
+			"gamma":                  "0.0000177",
+			"vega":                   "20.06494071",
+			"theta":                  "-4.26877439",
+			"predictedDeliveryPrice": "0",
+			"change24h":              "0",
+		},
+	}
+	bytesBody, err := json.Marshal(respBody)
+	require.NoError(t, err)
+
+	server, teardown := testhelper.NewServer(
+		testhelper.WithHandlerOption(path, method, status, bytesBody),
+	)
+	defer teardown()
+
+	client := NewTestClient().
+		WithBaseURL(server.URL)
+
+	resp, err := client.Derivative().UnifiedMargin().DerivativesTickersForOption(param)
+	require.NoError(t, err)
+
+	require.NotNil(t, resp)
+	assert.Equal(t, respBody["result"].(map[string]string)["category"], string(resp.Result.Category))
+	assert.Equal(t, respBody["result"].(map[string]string)["symbol"], string(resp.Result.Symbol))
+}
