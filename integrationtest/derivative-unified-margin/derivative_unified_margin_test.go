1f9bc34af64c61dd5c34acb81016c230b19fa423
hirokisan <dev@hirokiueno.com>
2022-11-26 08:47:48 +0900

    test: integration

--- integrationtest/derivative-unified-margin/derivative_unified_margin_test.go
+++ integrationtest/derivative-unified-margin/derivative_unified_margin_test.go
@@ -66,3 +66,16 @@ func TestDerivativesTickersForOption(t *testing.T) {
 		testhelper.UpdateFile(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
 	}
 }
+
+func TestDerivativesInstruments(t *testing.T) {
+	client := bybit.NewTestClient()
+	res, err := client.Derivative().UnifiedMargin().DerivativesInstruments(bybit.DerivativesInstrumentsParam{
+		Category: bybit.CategoryDerivativeLinear,
+	})
+	require.NoError(t, err)
+	{
+		goldenFilename := "./testdata/derivatives-public-instruments-info.json"
+		testhelper.Compare(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
+		testhelper.UpdateFile(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
+	}
+}
