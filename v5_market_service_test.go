4c2a26c059a9f64455701082eb6ad41b242b77ff
hirokisan <dev@hirokiueno.com>
2023-02-20 22:31:06 +0900

    test: unit

--- v5_market_service_test.go
+++ v5_market_service_test.go
@@ -55,3 +55,51 @@ func TestV5MarketGetKline(t *testing.T) {
 	assert.Equal(t, respBody["result"].(map[string]interface{})["symbol"], string(resp.Result.Symbol))
 	assert.Equal(t, respBody["result"].(map[string]interface{})["list"].([][]string)[0][0], resp.Result.List[0].StartTime)
 }
+
+func TestV5Market_GetMarkPriceKline(t *testing.T) {
+	param := V5GetMarkPriceKlineParam{
+		Category: CategoryV5Linear,
+		Symbol:   SymbolV5BTCUSDT,
+		Interval: IntervalD,
+	}
+
+	path := "/v5/market/mark-price-kline"
+	method := http.MethodGet
+	status := http.StatusOK
+	respBody := map[string]interface{}{
+		"result": map[string]interface{}{
+			"category": "linear",
+			"symbol":   "BTCUSDT",
+			"list": [][]string{
+				{
+					"1676851200000",
+					"24279.66",
+					"24978",
+					"23851.48",
+					"24881.63",
+				},
+			},
+		},
+	}
+	bytesBody, err := json.Marshal(respBody)
+	require.NoError(t, err)
+
+	server, teardown := testhelper.NewServer(
+		testhelper.WithHandlerOption(path, method, status, bytesBody),
+	)
+	defer teardown()
+
+	client := NewTestClient().
+		WithBaseURL(server.URL)
+
+	resp, err := client.V5().Market().GetMarkPriceKline(param)
+	require.NoError(t, err)
+
+	require.NotNil(t, resp)
+	assert.Equal(t, respBody["result"].(map[string]interface{})["symbol"], string(resp.Result.Symbol))
+	assert.Equal(t, respBody["result"].(map[string]interface{})["list"].([][]string)[0][0], resp.Result.List[0].StartTime)
+	assert.Equal(t, respBody["result"].(map[string]interface{})["list"].([][]string)[0][1], resp.Result.List[0].Open)
+	assert.Equal(t, respBody["result"].(map[string]interface{})["list"].([][]string)[0][2], resp.Result.List[0].High)
+	assert.Equal(t, respBody["result"].(map[string]interface{})["list"].([][]string)[0][3], resp.Result.List[0].Low)
+	assert.Equal(t, respBody["result"].(map[string]interface{})["list"].([][]string)[0][4], resp.Result.List[0].Close)
+}
