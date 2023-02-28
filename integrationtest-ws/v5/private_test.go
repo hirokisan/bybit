d4c445926fbded4461ba26bc44d2d87ba8b6029a
hirokisan <dev@hirokiueno.com>
2023-02-28 20:28:00 +0900

    test(integration): v5 ws private wallet

--- integrationtest-ws/v5/private_test.go
+++ integrationtest-ws/v5/private_test.go
@@ -32,3 +32,25 @@ func TestV5Private_Position(t *testing.T) {
 	// When you make a change to a position, the change is recorded.
 	// After recorded, please stop manually.
 }
+
+func TestV5Private_Wallet(t *testing.T) {
+	wsClient := bybit.NewTestWebsocketClient().WithAuthFromEnv()
+	svc, err := wsClient.V5().Private()
+	require.NoError(t, err)
+
+	require.NoError(t, svc.Subscribe())
+
+	_, err = svc.SubscribeWallet(
+		func(response bybit.V5WebsocketPrivateWalletResponse) error {
+			goldenFilename := "./testdata/private-v5-wallet.json"
+			testhelper.Compare(t, goldenFilename, testhelper.ConvertToJSON(response))
+			testhelper.UpdateFile(t, goldenFilename, testhelper.ConvertToJSON(response))
+			return nil
+		},
+	)
+	require.NoError(t, err)
+
+	require.NoError(t, svc.Start(context.Background(), nil))
+	// When you make a change to a position, the change is recorded.
+	// After recorded, please stop manually.
+}
