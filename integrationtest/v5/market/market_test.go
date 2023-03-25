c258c89b6f79ba5c64d934b25bda4157bb2c8048
hirokisan <dev@hirokiueno.com>
2023-03-25 09:28:08 +0900

    feat: v5 get funding rate history (#107)
    * feat: implement
    
    * test: integration
    
    * test: unit
    
    * docs: update

--- integrationtest/v5/market/market_test.go
+++ integrationtest/v5/market/market_test.go
@@ -159,3 +159,19 @@ func TestGetTickers(t *testing.T) {
 		}
 	}
 }
+
+func TestGetFundingRateHistory(t *testing.T) {
+	client := bybit.NewTestClient()
+	limit := 5
+	res, err := client.V5().Market().GetFundingRateHistory(bybit.V5GetFundingRateHistoryParam{
+		Category: bybit.CategoryV5Linear,
+		Symbol:   bybit.SymbolV5BTCUSDT,
+		Limit:    &limit,
+	})
+	require.NoError(t, err)
+	{
+		goldenFilename := "./testdata/v5-market-get-funding-rate-history.json"
+		testhelper.Compare(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
+		testhelper.UpdateFile(t, goldenFilename, testhelper.ConvertToJSON(res.Result))
+	}
+}
