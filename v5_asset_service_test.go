38ff125275e741203a062889eea146c9a03c2969
hirokisan <dev@hirokiueno.com>
2023-04-09 21:03:30 +0900

    test: unit

---
+++ v5_asset_service_test.go
@@ -0,0 +1,90 @@
+package bybit
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/hirokisan/bybit/v2/testhelper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestV5Asset_GetInternalTransferRecords(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		param := V5GetInternalTransferRecordsParam{}
+
+		path := "/v5/asset/transfer/query-inter-transfer-list"
+		method := http.MethodGet
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"list": []map[string]interface{}{
+					{
+						"transferId":      "selfTransfer_5ce5b8d9-8477-4bc6-91a4-9a98dad6dc65",
+						"coin":            "BTC",
+						"amount":          "0.1",
+						"fromAccountType": "SPOT",
+						"toAccountType":   "CONTRACT",
+						"timestamp":       "1637939106000",
+						"status":          "SUCCESS",
+					},
+				},
+				"nextPageCursor": "eyJtaW5JRCI6MTYyMjgwLCJtYXhJRCI6MTYyMjgwfQ==",
+			},
+		}
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL).
+			WithAuth("test", "test")
+
+		resp, err := client.V5().Asset().GetInternalTransferRecords(param)
+		require.NoError(t, err)
+
+		require.NotNil(t, resp)
+		testhelper.Compare(t, respBody["result"], resp.Result)
+	})
+	t.Run("authentication required", func(t *testing.T) {
+		param := V5GetInternalTransferRecordsParam{}
+
+		path := "/v5/asset/transfer/query-inter-transfer-list"
+		method := http.MethodGet
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"list": []map[string]interface{}{
+					{
+						"transferId":      "selfTransfer_5ce5b8d9-8477-4bc6-91a4-9a98dad6dc65",
+						"coin":            "BTC",
+						"amount":          "0.1",
+						"fromAccountType": "SPOT",
+						"toAccountType":   "CONTRACT",
+						"timestamp":       "1637939106000",
+						"status":          "SUCCESS",
+					},
+				},
+				"nextPageCursor": "eyJtaW5JRCI6MTYyMjgwLCJtYXhJRCI6MTYyMjgwfQ==",
+			},
+		}
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL)
+
+		_, err = client.V5().Asset().GetInternalTransferRecords(param)
+		assert.Error(t, err)
+	})
+}
