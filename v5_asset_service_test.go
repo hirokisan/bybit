29d19089dfbd8d7f7f16f9d81a8b087c286c93fb
hirokisan <dev@hirokiueno.com>
2023-04-18 21:44:56 +0900

    test: unit

--- v5_asset_service_test.go
+++ v5_asset_service_test.go
@@ -89,6 +89,95 @@ func TestV5Asset_GetInternalTransferRecords(t *testing.T) {
 	})
 }
 
+func GetDepositRecords(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		param := V5GetDepositRecordsParam{}
+
+		path := "/v5/asset/deposit/query-record"
+		method := http.MethodGet
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"rows": []map[string]interface{}{
+					{
+						"coin":          "USDT",
+						"chain":         "ETH",
+						"amount":        "10000",
+						"txID":          "skip-notification-scene-test-amount-202304160106-146940-USDT",
+						"status":        3,
+						"toAddress":     "test-amount-address",
+						"tag":           "",
+						"depositFee":    "",
+						"successAt":     "1681607166000",
+						"confirmations": "10000",
+						"txIndex":       "",
+						"blockHash":     "",
+					},
+				},
+				"nextPageCursor": "eyJtaW5JRCI6MTEzMzUzNSwibWF4SUQiOjExMzM1MzV9",
+			},
+		}
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL).
+			WithAuth("test", "test")
+
+		resp, err := client.V5().Asset().GetDepositRecords(param)
+		require.NoError(t, err)
+
+		require.NotNil(t, resp)
+		testhelper.Compare(t, respBody["result"], resp.Result)
+	})
+	t.Run("authentication required", func(t *testing.T) {
+		param := V5GetDepositRecordsParam{}
+
+		path := "/v5/asset/deposit/query-record"
+		method := http.MethodGet
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"rows": []map[string]interface{}{
+					{
+						"coin":          "USDT",
+						"chain":         "ETH",
+						"amount":        "10000",
+						"txID":          "skip-notification-scene-test-amount-202304160106-146940-USDT",
+						"status":        3,
+						"toAddress":     "test-amount-address",
+						"tag":           "",
+						"depositFee":    "",
+						"successAt":     "1681607166000",
+						"confirmations": "10000",
+						"txIndex":       "",
+						"blockHash":     "",
+					},
+				},
+				"nextPageCursor": "eyJtaW5JRCI6MTEzMzUzNSwibWF4SUQiOjExMzM1MzV9",
+			},
+		}
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL)
+
+		_, err = client.V5().Asset().GetDepositRecords(param)
+		assert.Error(t, err)
+	})
+}
+
 func GetInternalDepositRecords(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		param := V5GetInternalDepositRecordsParam{}
