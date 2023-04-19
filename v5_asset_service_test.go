c939fa01f9fd4ba866d7fe5576009c76e81d9020
hirokisan <dev@hirokiueno.com>
2023-04-19 21:29:20 +0900

    feat: v5 get sub deposit records (#123)
    * feat: implement
    
    * test: integration
    
    * test: unit
    
    * docs: update

--- v5_asset_service_test.go
+++ v5_asset_service_test.go
@@ -178,6 +178,65 @@ func GetDepositRecords(t *testing.T) {
 	})
 }
 
+func GetSubDepositRecords(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		param := V5GetSubDepositRecordsParam{}
+
+		path := "/v5/asset/deposit/query-sub-member-record"
+		method := http.MethodGet
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"rows":           []map[string]interface{}{},
+				"nextPageCursor": "",
+			},
+		}
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL).
+			WithAuth("test", "test")
+
+		resp, err := client.V5().Asset().GetSubDepositRecords(param)
+		require.NoError(t, err)
+
+		require.NotNil(t, resp)
+		testhelper.Compare(t, respBody["result"], resp.Result)
+	})
+	t.Run("authentication required", func(t *testing.T) {
+		param := V5GetSubDepositRecordsParam{}
+
+		path := "/v5/asset/deposit/query-sub-member-record"
+		method := http.MethodGet
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"rows":           []map[string]interface{}{},
+				"nextPageCursor": "",
+			},
+		}
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL)
+
+		_, err = client.V5().Asset().GetSubDepositRecords(param)
+		assert.Error(t, err)
+	})
+}
+
 func GetInternalDepositRecords(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		param := V5GetInternalDepositRecordsParam{}
