92f531f491aa7fd1e60bf78fac9958a9a1674528
hirokisan <dev@hirokiueno.com>
2023-04-23 11:18:33 +0900

    test: unit

--- v5_asset_service_test.go
+++ v5_asset_service_test.go
@@ -295,3 +295,62 @@ func GetInternalDepositRecords(t *testing.T) {
 		assert.Error(t, err)
 	})
 }
+
+func TestGetWithdrawalRecords(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		param := V5GetWithdrawalRecordsParam{}
+
+		path := "/v5/asset/withdraw/query-record"
+		method := http.MethodGet
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"rows":           []map[string]interface{}{},
+				"nextPageCursor": "",
+			},
+		}
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL).
+			WithAuth("test", "test")
+
+		resp, err := client.V5().Asset().GetWithdrawalRecords(param)
+		require.NoError(t, err)
+
+		require.NotNil(t, resp)
+		testhelper.Compare(t, respBody["result"], resp.Result)
+	})
+	t.Run("authentication required", func(t *testing.T) {
+		param := V5GetWithdrawalRecordsParam{}
+
+		path := "/v5/asset/withdraw/query-record"
+		method := http.MethodGet
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"rows":           []map[string]interface{}{},
+				"nextPageCursor": "",
+			},
+		}
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL)
+
+		_, err = client.V5().Asset().GetWithdrawalRecords(param)
+		assert.Error(t, err)
+	})
+}
