2a9c80b757a96a3c10503feee66fd56224f276da
kbnchk <kabanchik136@gmail.com>
2023-09-01 12:01:54 +0300

    added v5().Execution().GetExecutionList() method

--- v5_execution_service.go
+++ v5_execution_service.go
@@ -1,10 +1,85 @@
 package bybit
 
+import "github.com/google/go-querystring/query"
+
 // V5ExecutionServiceI :
 type V5ExecutionServiceI interface {
+	GetExecutionList(V5GetExecutionParam) (*V5GetExecutionListResponse, error)
 }
 
 // V5ExecutionService :
 type V5ExecutionService struct {
 	client *Client
+}
+
+// V5GetExecutionParam :
+type V5GetExecutionParam struct {
+	Category CategoryV5 `url:"category"`
+
+	Symbol      *SymbolV5   `url:"symbol,omitempty"`
+	OrderId     *string     `url:"orderId,omitempty"`
+	OrderLinkId *string     `url:"orderLinkId,omitempty"`
+	BaseCoin    *Coin       `url:"baseCoin,omitempty"`
+	StartTime   *int        `url:"startTime,omitempty"`
+	EndTime     *int        `url:"endTime,omitempty"`
+	ExecType    *ExecTypeV5 `url:"execType,omitempty"`
+	Limit       *int        `url:"limit,omitempty"`
+	Cursor      *string     `url:"cursor,omitempty"`
+}
+
+// V5GetExecutionListResponse :
+type V5GetExecutionListResponse struct {
+	CommonV5Response `json:",inline"`
+	Result           V5GetExecutionListResult `json:"result"`
+}
+
+// V5GetExecutionListResult :
+type V5GetExecutionListResult struct {
+	NextPageCursor string                   `json:"nextPageCursor"`
+	Category       string                   `json:"category"`
+	List           []V5GetExecutionListItem `json:"list"`
+}
+
+// V5GetExecutionListItem :
+type V5GetExecutionListItem struct {
+	Symbol          SymbolV5   `json:"symbol"`
+	OrderID         string     `json:"orderId"`
+	OrderLinkId     string     `json:"orderLinkId"`
+	Side            Side       `json:"side"`
+	OrderPrice      string     `json:"orderPrice"`
+	OrderQty        string     `json:"orderQty"`
+	LeavesQty       string     `json:"leavesQty"`
+	OrderType       OrderType  `json:"orderType"`
+	StopOrderType   string     `json:"stopOrderType"`
+	ExecFee         string     `json:"execFee"`
+	ExecID          string     `json:"execId"`
+	ExecPrice       string     `json:"execPrice"`
+	ExecQty         string     `json:"execQty"`
+	ExecType        ExecTypeV5 `json:"execType"`
+	ExecValue       string     `json:"execValue"`
+	ExecTime        string     `json:"execTime"`
+	IsMaker         bool       `json:"isMaker"`
+	FeeRate         string     `json:"feeRate"`
+	TradeIv         string     `json:"tradeIv"`
+	MarkIv          string     `json:"markIv"`
+	MarkPrice       string     `json:"markPrice"`
+	IndexPrice      string     `json:"indexPrice"`
+	UnderlyingPrice string     `json:"underlyingPrice"`
+	BlockTradeId    string     `json:"blockTradeId"`
+	ClosedSize      string     `json:"closedSize"`
+}
+
+func (s *V5ExecutionService) GetExecutionList(param V5GetExecutionParam) (*V5GetExecutionListResponse, error) {
+	var res V5GetExecutionListResponse
+
+	queryString, err := query.Values(param)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.client.getV5Privately("/v5/execution/list", queryString, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
 }
---
+++ v5_execution_service_test.go
@@ -0,0 +1,133 @@
+package bybit
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/hirokisan/bybit/v2/testhelper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestV5ExecutionService_GetExecutionList(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		param := V5GetExecutionParam{
+			Category: CategoryV5Linear,
+		}
+
+		path := "/v5/execution/list"
+		method := http.MethodGet
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"category":       "linear",
+				"nextPageCursor": "",
+				"list": []map[string]interface{}{
+					{
+						"symbol":          "BTCUSDT",
+						"orderId":         "orderId",
+						"orderLinkId":     "orderLinkId",
+						"side":            "Buy",
+						"orderPrice":      "24617.44",
+						"orderQty":        "0.01",
+						"leavesQty":       "0.5",
+						"orderType":       "Limit",
+						"stopOrderType":   "tpslOrder",
+						"execFee":         "0.01385448",
+						"execId":          "execId",
+						"execPrice":       "23089.30",
+						"execQty":         "0.001",
+						"execType":        "Trade",
+						"execValue":       "34.5",
+						"execTime":        "1693555728000",
+						"isMaker":         true,
+						"feeRate":         "0.03455",
+						"tradeIv":         "0.0035",
+						"markIv":          "0.0352",
+						"markPrice":       "23239.10",
+						"indexPrice":      "20334.55",
+						"underlyingPrice": "21243.21",
+						"blockTradeId":    "blockTradeId",
+						"closedSize":      "233",
+					},
+				},
+			},
+		}
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL).
+			WithAuth("test", "test")
+
+		resp, err := client.V5().Execution().GetExecutionList(param)
+		require.NoError(t, err)
+
+		require.NotNil(t, resp)
+		testhelper.Compare(t, respBody["result"], resp.Result)
+	})
+	t.Run("authentication required", func(t *testing.T) {
+		param := V5GetExecutionParam{
+			Category: CategoryV5Linear,
+		}
+
+		path := "/v5/execution/list"
+		method := http.MethodGet
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"category":       "linear",
+				"nextPageCursor": "",
+				"list": []map[string]interface{}{
+					{
+						"symbol":          "BTCUSDT",
+						"orderId":         "orderId",
+						"orderLinkId":     "orderLinkId",
+						"side":            "Buy",
+						"orderPrice":      "24617.44",
+						"orderQty":        "0.01",
+						"leavesQty":       "0.5",
+						"orderType":       "Limit",
+						"stopOrderType":   "tpslOrder",
+						"execFee":         "0.01385448",
+						"execId":          "execId",
+						"execPrice":       "23089.30",
+						"execQty":         "0.001",
+						"execType":        "Trade",
+						"execValue":       "34.5",
+						"execTime":        "1693555728000",
+						"isMaker":         true,
+						"feeRate":         "0.03455",
+						"tradeIv":         "0.0035",
+						"markIv":          "0.0352",
+						"markPrice":       "23239.10",
+						"indexPrice":      "20334.55",
+						"underlyingPrice": "21243.21",
+						"blockTradeId":    "blockTradeId",
+						"closedSize":      "233",
+					},
+				},
+			},
+		}
+
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL)
+
+		_, err = client.V5().Execution().GetExecutionList(param)
+		assert.Error(t, err)
+	})
+}
