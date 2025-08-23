21a135320e45f9e8ec13074e9857c118483cd104
jjkoh95 <jjkoh95@gmail.com>
2025-08-23 12:49:28 +0800

    Add `Create Sub UID`, `Create Sub UID API Key`, `Get Sub UID List (Limited)` (#212)

--- v5_user_service.go
+++ v5_user_service.go
@@ -1,18 +1,118 @@
 package bybit
 
 import (
+	"encoding/json"
 	"net/url"
 	"time"
 )
 
 // V5UserServiceI :
 type V5UserServiceI interface {
+	CreateSubUID(param V5CreateSubUIDParam) (*V5CreateSubUIDResponse, error)
+	GetSubUIDList() (*V5GetSubUIDListResponse, error)
+	CreateSubUIDAPIKey(param V5CreateSubUIDAPIKeyParam) (*V5CreateSubUIDAPIKeyResponse, error)
 	GetAPIKey() (*V5APIKeyResponse, error)
 }
 
 // V5UserService :
 type V5UserService struct {
 	client *Client
+}
+
+// V5CreateSubUIDParam :
+type V5CreateSubUIDParam struct {
+	Username   string  `json:"username"`
+	MemberType int     `json:"memberType"`
+	Password   *string `json:"password,omitempty"`
+	Switch     *int    `json:"switch,omitempty"`
+	Note       *string `json:"note,omitempty"`
+}
+
+// V5CreateSubUIDResponse :
+type V5CreateSubUIDResponse struct {
+	CommonV5Response `json:",inline"`
+	Result           V5CreateSubUIDResult `json:"result"`
+}
+
+// V5CreateSubUIDResult :
+type V5CreateSubUIDResult struct {
+	UID        string `json:"uid"`
+	Username   string `json:"username"`
+	MemberType int    `json:"memberType"`
+	Status     int    `json:"status"`
+	Remark     string `json:"remark"`
+}
+
+// V5GetSubUIDListResponse :
+type V5GetSubUIDListResponse struct {
+	CommonV5Response `json:",inline"`
+	Result           V5GetSubUIDListResult `json:"result"`
+}
+
+// V5GetSubUIDListResult :
+type V5GetSubUIDListResult struct {
+	SubMembers []V5SubMember `json:"subMembers"`
+}
+
+// V5SubMember :
+type V5SubMember struct {
+	UID         string `json:"uid"`
+	Username    string `json:"username"`
+	MemberType  int    `json:"memberType"`
+	Status      int    `json:"status"`
+	AccountMode int    `json:"accountMode"`
+	Remark      string `json:"remark"`
+}
+
+// V5CreateSubUIDAPIKeyParam :
+type V5CreateSubUIDAPIKeyParam struct {
+	Subuid      int                     `json:"subuid"`
+	ReadOnly    int                     `json:"readOnly"`
+	Note        *string                 `json:"note,omitempty"`
+	Ips         *string                 `json:"ips,omitempty"`
+	Permissions V5APIKeyPermissionsParam `json:"permissions"`
+}
+
+// V5APIKeyPermissionsParam :
+type V5APIKeyPermissionsParam struct {
+	ContractTrade []string `json:"ContractTrade,omitempty"`
+	Spot          []string `json:"Spot,omitempty"`
+	Wallet        []string `json:"Wallet,omitempty"`
+	Options       []string `json:"Options,omitempty"`
+	Derivatives   []string `json:"Derivatives,omitempty"`
+	CopyTrading   []string `json:"CopyTrading,omitempty"`
+	BlockTrade    []string `json:"BlockTrade,omitempty"`
+	Exchange      []string `json:"Exchange,omitempty"`
+	NFT           []string `json:"NFT,omitempty"`
+}
+
+// V5CreateSubUIDAPIKeyResponse :
+type V5CreateSubUIDAPIKeyResponse struct {
+	CommonV5Response `json:",inline"`
+	Result           V5CreateSubUIDAPIKeyResult `json:"result"`
+}
+
+// V5CreateSubUIDAPIKeyResult :
+type V5CreateSubUIDAPIKeyResult struct {
+	ID          string `json:"id"`
+	Note        string `json:"note"`
+	APIKey      string `json:"apiKey"`
+	ReadOnly    int    `json:"readOnly"`
+	Secret      string `json:"secret"`
+	Permissions V5APIKeyPermissions `json:"permissions"`
+}
+
+// V5APIKeyPermissions :
+type V5APIKeyPermissions struct {
+	ContractTrade []string `json:"ContractTrade"`
+	Spot          []string `json:"Spot"`
+	Wallet        []string `json:"Wallet"`
+	Options       []string `json:"Options"`
+	Derivatives   []string `json:"Derivatives"`
+	CopyTrading   []string `json:"CopyTrading"`
+	BlockTrade    []string `json:"BlockTrade"`
+	Exchange      []string `json:"Exchange"`
+	NFT           []string `json:"NFT"`
 }
 
 // V5APIKeyResponse :
@@ -51,6 +151,55 @@ type V5ApiKeyResult struct {
 	VipLevel      string    `json:"vipLevel"`
 	MktMakerLevel string    `json:"mktMakerLevel"`
 	AffiliateID   int       `json:"affiliateID"`
+}
+
+// CreateSubUID :
+func (s *V5UserService) CreateSubUID(param V5CreateSubUIDParam) (*V5CreateSubUIDResponse, error) {
+	var (
+		res V5CreateSubUIDResponse
+	)
+
+	body, err := json.Marshal(param)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.client.postV5JSON("/v5/user/create-sub-member", body, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+// GetSubUIDList :
+func (s *V5UserService) GetSubUIDList() (*V5GetSubUIDListResponse, error) {
+	var (
+		res V5GetSubUIDListResponse
+	)
+
+	if err := s.client.getV5Privately("/v5/user/query-sub-members", url.Values{}, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+// CreateSubUIDAPIKey :
+func (s *V5UserService) CreateSubUIDAPIKey(param V5CreateSubUIDAPIKeyParam) (*V5CreateSubUIDAPIKeyResponse, error) {
+	var (
+		res V5CreateSubUIDAPIKeyResponse
+	)
+
+	body, err := json.Marshal(param)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.client.postV5JSON("/v5/user/create-sub-api", body, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
 }
 
 // GetAPIKey :
--- v5_user_service_test.go
+++ v5_user_service_test.go
@@ -9,6 +9,160 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestV5User_CreateSubUID(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		path := "/v5/user/create-sub-member"
+		method := http.MethodPost
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"uid":        "53888000",
+				"username":   "test0001",
+				"memberType": 1,
+				"status":     1,
+				"remark":     "test account",
+			},
+		}
+
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL).
+			WithAuth("test", "test")
+
+		password := "Password123"
+		switchVal := 1
+		note := "test account"
+		param := V5CreateSubUIDParam{
+			Username:   "test0001",
+			MemberType: 1,
+			Password:   &password,
+			Switch:     &switchVal,
+			Note:       &note,
+		}
+
+		resp, err := client.V5().User().CreateSubUID(param)
+		require.NoError(t, err)
+
+		require.NotNil(t, resp)
+		testhelper.Compare(t, respBody["result"], resp.Result)
+	})
+}
+
+func TestV5User_GetSubUIDList(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		path := "/v5/user/query-sub-members"
+		method := http.MethodGet
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"subMembers": []map[string]interface{}{
+					{
+						"uid":         "53888000",
+						"username":    "test0001",
+						"memberType":  1,
+						"status":      1,
+						"accountMode": 3,
+						"remark":      "test account",
+					},
+					{
+						"uid":         "53888001",
+						"username":    "test0002",
+						"memberType":  1,
+						"status":      1,
+						"accountMode": 3,
+						"remark":      "",
+					},
+				},
+			},
+		}
+
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL).
+			WithAuth("test", "test")
+
+		resp, err := client.V5().User().GetSubUIDList()
+		require.NoError(t, err)
+
+		require.NotNil(t, resp)
+		testhelper.Compare(t, respBody["result"], resp.Result)
+	})
+}
+
+func TestV5User_CreateSubUIDAPIKey(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		path := "/v5/user/create-sub-api"
+		method := http.MethodPost
+		status := http.StatusOK
+		respBody := map[string]interface{}{
+			"result": map[string]interface{}{
+				"id":       "16651283",
+				"note":     "test api key",
+				"apiKey":   "XXXXXXXXXX",
+				"readOnly": 1,
+				"secret":   "YYYYYYYYYY",
+				"permissions": map[string]interface{}{
+					"ContractTrade": []interface{}{"Order", "Position"},
+					"Spot":          []interface{}{"SpotTrade"},
+					"Wallet":        []interface{}{"AccountTransfer"},
+					"Options":       []interface{}{},
+					"Derivatives":   []interface{}{},
+					"CopyTrading":   []interface{}{},
+					"BlockTrade":    []interface{}{},
+					"Exchange":      []interface{}{},
+					"NFT":           []interface{}{},
+				},
+			},
+		}
+
+		bytesBody, err := json.Marshal(respBody)
+		require.NoError(t, err)
+
+		server, teardown := testhelper.NewServer(
+			testhelper.WithHandlerOption(path, method, status, bytesBody),
+		)
+		defer teardown()
+
+		client := NewTestClient().
+			WithBaseURL(server.URL).
+			WithAuth("test", "test")
+
+		note := "test api key"
+		ips := "192.168.0.1,192.168.0.2"
+		param := V5CreateSubUIDAPIKeyParam{
+			Subuid:   53888000,
+			ReadOnly: 1,
+			Note:     &note,
+			Ips:      &ips,
+			Permissions: V5APIKeyPermissionsParam{
+				ContractTrade: []string{"Order", "Position"},
+				Spot:          []string{"SpotTrade"},
+				Wallet:        []string{"AccountTransfer"},
+			},
+		}
+
+		resp, err := client.V5().User().CreateSubUIDAPIKey(param)
+		require.NoError(t, err)
+
+		require.NotNil(t, resp)
+		testhelper.Compare(t, respBody["result"], resp.Result)
+	})
+}
+
 func TestV5User_GetAPIKey(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		path := "/v5/user/query-api"
